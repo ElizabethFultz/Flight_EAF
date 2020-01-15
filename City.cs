c89d10cfaada8b400844a8cfcf38892f6bd93fa2
ElizabethFultz <efultz@bellarmine.edu>
2020-01-15 18:16:56 -0500

    Inital Commit

---
+++ City.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * This class represents a City with
+ * a name, lat and long.
+ * @author Rob Kelley
+ * @version 1.0;
+ * Lab04 - Solution
+ * SP19
+ */
+
+namespace Flight_EAF
+{
+    class City
+    {
+		private readonly String name;
+		private readonly double latitude;
+		private readonly double longitude;
+
+		/**
+		 * Empty-argument constructor to put 
+		 * object into a consistent state.
+		 */
+		private City()
+		{
+			name = "";
+			latitude = 0.0;
+			longitude = 0.0;
+		}//end constructor
+
+		/**
+		 * Constructor to instantiate the object.
+		 * @param name
+		 * @param latitude
+		 * @param longitude
+		 */
+		public City(String name, double latitude, double longitude)
+		{
+			this.name = name;
+			this.latitude = latitude;
+			this.longitude = longitude;
+		}//end constructor
+
+		/**
+		 * Getter for name
+		 * @return
+		 */
+		public String GetName()
+		{
+			return name;
+		}//end GetName
+
+		/**
+		 * Getter for latitude
+		 * @return
+		 */
+		public double GetLatitude()
+		{
+			return latitude;
+		}//end GetName
+
+		/**
+		 * Getter for longitude
+		 * @return
+		 */
+		public double GetLongitude()
+		{
+			return longitude;
+		}//end GetLongitude
+
+		public override String ToString()
+			{
+				return "City [name=" + name + ", latitude=" + latitude + ", longitude=" + longitude + "]";
+			}
+	}
+}
---
+++ Flight.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * This object represents a flight
+ * @author Rob Kelley
+ * @version 1.0
+ * Lab04 - Solution
+ * SP19
+ */
+
+namespace Flight_EAF
+{
+    class Flight
+    {
+
+		private String airLineName;
+		private City originCity;
+		private City destinationCity;
+		private String flightNumber;
+
+		/**
+		 * Empty-argument constructor to put 
+		 * the object into a consistent state.
+		 */
+		public Flight()
+		{
+			airLineName = null;
+			originCity = null;
+			destinationCity = null;
+			flightNumber = null;
+		}//end constructor
+
+		/**
+		 * Preferred constructor for this object
+		 * @param aln - airline number
+		 * @param fn - flight number
+		 * @param o - origin city
+		 * @param d - destination city
+		 */
+		public Flight(String aln, String fn, City o, City d)
+		{
+
+			SetAirLineName(aln);
+			SetFlightNumber(fn);
+			SetOriginCity(o);
+			SetDestinationCity(d);
+
+		}//end constructor
+
+		/**
+		 * Method uses the haversine formulae
+		 * to calculate the distance around the 
+		 * globe from one city to another.
+		 * @return - distance in miles
+		 */
+		public double CalcDistanceToFly()
+		{
+
+			double R = 6371000;
+			double lat1 = originCity.GetLatitude();
+			double lat2 = destinationCity.GetLatitude();
+			double lon1 = originCity.GetLongitude();
+			double lon2 = destinationCity.GetLongitude();
+
+			double lat1Radians = (Math.PI/180.0)*lat1;
+			double lat2Radians = (Math.PI/180.0)*lat2;
+			double lon1Radians = (Math.PI/180.0)*lon1;
+			double lon2Radians = (Math.PI/180.0)*lon2;
+
+			double deltaLat = (Math.PI/180.0)*(lat2 - lat1);
+			double deltaLon = (Math.PI/180.0)*(lon2 - lon1);
+
+			double a = Math.Pow(Math.Sin(deltaLat / 2), 2) + Math.Cos(lat1Radians) * Math.Cos(lat2Radians) * Math.Pow(Math.Sin(deltaLon / 2), 2);
+
+			double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+			double distance = R * c;
+
+			return distance * 0.000621371;
+		}//end 
+
+		/**
+		 * This method will return all of the flight
+		 * details including the airline, flight number,
+		 * and distance between two city objects.
+		 * @return -String representing the flight details
+		 */
+		public String PrintFlightDetails()
+		{ 
+
+			StringBuilder sb = new StringBuilder();
+
+			sb.Append(airLineName + " " + flightNumber + "\n");
+			sb.Append(originCity.GetName() + " to " + destinationCity.GetName() + "\n");
+			sb.Append("Distance: " + this.CalcDistanceToFly() + " miles.\n");
+
+			return sb.ToString();
+		}//end PrintFlightDetails
+
+		/**
+		 * getter for airLineName
+		 * @return
+		 */
+		public String GetAirLineName()
+		{
+			return airLineName;
+		}//end GetAirline
+
+		/**
+		 * Setter for airLineName
+		 * @param airLineName
+		 */
+		public void SetAirLineName(String airLineName)
+		{
+			this.airLineName = airLineName;
+		}//end SetAirLineName
+
+		/**
+		 * Getter for flightNumber
+		 * @return
+		 */
+		public String GetFlightNumber()
+		{
+			return flightNumber;
+		}//end GetFlightNumber
+
+		/**
+		 * Setter for flightNumber
+		 * @param flightNumber
+		 */
+		public void SetFlightNumber(String flightNumber)
+		{
+			this.flightNumber = flightNumber;
+		}//end SetFlightNumber
+
+		/**
+		 * Getter for originCity
+		 * @return
+		 */
+		public City GetOriginCity()
+		{
+			return originCity;
+		}//end GetOriginCity
+
+		/**
+		 * Setter for originCity
+		 * @param originCity
+		 */
+		public void SetOriginCity(City originCity)
+		{
+			this.originCity = originCity;
+		}//end SetOriginCity
+
+		/**
+		 * Getter for destinationCity
+		 * @return
+		 */
+		public City GetDestinationCity()
+		{
+			return destinationCity;
+		}//end GetDestinationCity
+
+		/**
+		 * Setter for destinationCity
+		 * @param destinationCity
+		 */
+		public void SetDestinationCity(City destinationCity)
+		{
+			this.destinationCity = destinationCity;
+		}//end SetDestinationCity
+
+		public override String ToString()
+			{
+				return "Flight [airLineName=" + airLineName + ", originCity=" + originCity + ", destinationCity="
+						+ destinationCity + ", flightNumber=" + flightNumber + "]";
+			}//end toString
+
+	}
+}
